ba183cc1ec85c1753b8c84b86f9653fb8cde56c7
Lian <nail3105@gmail.com>
2019-09-07 17:28:08 -0600

    Scanning directory passed as parameter

--- main.c
+++ main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
-int main(int argc, char** argv){
+void scan_dir(char* dir_name);
 
-    printf("Hello World!\n");
+int main(int argc, char **argv)
+{
+
+    if (argc <= 1)
+    {
+        printf("\n[Error] Please include the directory to sync.\n\n");
+        return 1;
+    }
+
+    printf("Reading file: %s\n", argv[1]);
+
+    scan_dir(argv[1]);
+
     return 0;
+}
+
+void scan_dir(char *dir_name)
+{
+    DIR *d;
+    struct dirent *dir;
+
+    d = opendir(dir_name);
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
+    else
+    {
+        printf("Dir not open\n");
+    }
 }
