27c2f3f0b090e5d36398aa3afa99e1882108cc22
Lian <nail3105@gmail.com>
2019-09-15 14:35:02 -0600

    time exchange added

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <zmq.h>
 #include <assert.h>
 #include "arraylist.h"
@@ -9,7 +10,12 @@ struct ArrayList *history;
 struct ArrayList *files;
 struct ArrayList *changes;
 
+double time_diff;
+
 void update_local_history(char *dir);
+
+char *msg_recv(void *socket, int flag);
+int msg_send(void *socket, char *msg);
 
 int main(int argc, char **argv)
 {
@@ -21,25 +27,49 @@ int main(int argc, char **argv)
     }
 
     printf("\n====================================\n");
-    printf("Updating local history...");
+    printf(" ==> Updating local history...");
     printf("\n------------------------------------\n\n");
 
     update_local_history(argv[1]);
 
     printf("\n====================================\n");
-    printf("Initializing server...");
+    printf(" ==> Initializing server...");
     printf("\n------------------------------------\n\n");
 
     void *context = zmq_ctx_new();
 
     void *responder = zmq_socket(context, ZMQ_REP);
-    zmq_bind(responder, "tcp://*:5555");
+    int r = zmq_bind(responder, "tcp://*:5555");
     assert(responder);
+    assert(r == 0);
+
+    printf("--> Server ready listening in port :5555\n");
+
+    printf("\n====================================\n");
+    printf(" ==> Waiting for client...");
+    printf("\n------------------------------------\n\n");
+
+    char *request;
+    request = msg_recv(responder, 0);
+
+    time_t c_time = time(NULL);
+
+    time_t client_time;
+    sscanf(request, "%lu", &client_time);
+    free(request);
+
+    time_diff = difftime(c_time, client_time);
+
+    printf("--> Current time: %s\n", ctime(&c_time));
+    printf("--> Client's time: %s\n", ctime(&client_time));
+    printf("--> Time difference: %.f\n", time_diff);
+
+    char buffer[20];
+    sprintf(buffer, "%lu", (unsigned long)c_time);
+    msg_send(responder, buffer);
 
     zmq_close(responder);
     zmq_ctx_destroy(context);
-    return 0;
-
     return 0;
 }
 
@@ -60,9 +90,26 @@ void update_local_history(char *dir)
     for (i = 0; i < arraylist_size(changes); i++)
     {
         struct Change *change = (struct Change *)arraylist_get(changes, i);
-        printf(" -> File named %s was %s\n", change->file->name, change->type == 0 ? "created" : change->type == 1 ? "modified" : "deleted");
+        printf("\n -> File named %s was %s\n", change->file->name, change->type == 0 ? "created" : change->type == 1 ? "modified" : "deleted");
     }
 
-    printf("\n--> Saving directory history updated\n");
+    printf("--> Saving directory history updated\n");
     save_data(files);
+}
+
+char *msg_recv(void *socket, int flag)
+{
+    char buffer[256];
+    int size = zmq_recv(socket, buffer, 255, flag);
+    if (size == -1)
+        return NULL;
+    buffer[size] = '\0';
+    return strdup(buffer);
+}
+
+int msg_send(void *socket, char *msg)
+{
+    if (msg == NULL)
+        return zmq_send(socket, NULL, 0, 0);
+    return zmq_send(socket, msg, strlen(msg), 0);
 }
